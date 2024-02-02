d6e5a3bc386eabc87401185f432f04b19a57d5be
mbabaziemelyne <uwimbeme@gmail.com>
2024-02-02 22:45:28 +0200

    modifications

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -6,21 +6,28 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv('AUTH_TYPE')
-
-if AUTH_TYPE == "auth":
+if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -32,52 +39,31 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Handle a unauthorized access
-
-        Args:
-            error: Error catch
-
-        Return:
-            Info of the error
+    """ Request unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Handle a forbidden resource
-
-        Args:
-            error: Error catch
-
-        Return:
-            Info of the error
+    """ Request forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request() -> str:
-    """Execute before each request
-
-        Return:
-            String or nothing
+def before_request():
+    """ Before request
     """
-    if auth is None:
-        return
-
-    expath = ['/api/v1/status/',
-              '/api/v1/unauthorized/',
-              '/api/v1/forbidden/']
-
-    if not (auth.require_auth(request.path, expath)):
-        return
-
-    if (auth.authorization_header(request)) is None:
-        abort(401)
-
-    if (auth.current_user(request)) is None:
-        abort(403)
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,58 +1,49 @@
 #!/usr/bin/env python3
-""" Module of Users views
+""" Module of session auth views
 """
 from api.v1.views import app_views
-from flask import abort, jsonify, request, make_response
-from os import getenv
+from flask import abort, jsonify, request
 from models.user import User
-from typing import TypeVar, List
+from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """ Auth session Login
-
-        Return:
-            Sessioned with credentials
+    """ POST /auth_session/login
+    Return:
+     - User instance based on email
     """
     email = request.form.get('email')
-
+    password = request.form.get('password')
     if not email:
-        return make_response(jsonify({"error": "email missing"}), 400)
-
-    passwd = request.form.get('password')
-    if not passwd:
-        return make_response(jsonify({"error": "password missing"}), 400)
-
-    exist_user = User.search({"email": email})
-
-    if len(exist_user) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+    try:
+        users = User.search({'email': email})
+    except Exception:
         return jsonify({"error": "no user found for this email"}), 404
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    for u in users:
+        if not u.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth
+        session_id = auth.create_session(u.id)
+        out = jsonify(u.to_json())
+        out.set_cookie(getenv('SESSION_NAME'), session_id)
+        return out
+    return jsonify({"error": "no user found for this email"}), 404
 
-    from api.v1.app import auth
-    for user in exist_user:
-        if (user.is_valid_password(passwd)):
-            session_id = auth.create_session(user.id)
-            SESSION_NAME = getenv('SESSION_NAME')
-            response = make_response(user.to_json())
-            response.set_cookie(SESSION_NAME, session_id)
-            return response
 
-    return make_response(jsonify({"error": "wrong password"}), 401)
-
-
-@app_views.route('/auth_session/logout',
-                 methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout():
-    """ Logout of the session
-
-        Return:
-            Logout session
+    """ DELETE /auth_session/logout
+    Return:
+     - Empty json
     """
     from api.v1.app import auth
-    isdestroy = auth.destroy_session(request)
-
-    if isdestroy is False:
+    if not auth.destroy_session(request):
         abort(404)
-
     return jsonify({}), 200
